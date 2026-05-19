4d53e10072484d60ff4a91b9872b7f6ee410bfa6
Danesh Naik <naikdanesh2@gmail.com>
2026-05-19 22:36:45 +0530

    Fix for ui

--- vite.config.js
+++ vite.config.js
@@ -3,33 +3,34 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 import { resolve } from 'path'
 
-export default defineConfig(({ command }) => {
-  if (command === 'serve') {
+export default defineConfig(({ command, mode }) => {
+  if (mode === 'lib') {
     return {
       plugins: [react(), tailwindcss()],
-    }
-  }
-
-  return {
-    plugins: [react(), tailwindcss()],
-    build: {
-      lib: {
-        entry: resolve(__dirname, 'src/lib/index.js'),
-        name: 'DaneshUI',
-        fileName: (format) => `index.${format === 'es' ? 'mjs' : 'js'}`,
-        formats: ['es', 'cjs'],
-      },
-      rollupOptions: {
-        external: ['react', 'react-dom', 'react/jsx-runtime', 'daneshicons'],
-        output: {
-          globals: {
-            react: 'React',
-            'react-dom': 'ReactDOM',
-            'react/jsx-runtime': 'jsxRuntime',
-            daneshicons: 'DaneshIcons',
+      build: {
+        lib: {
+          entry: resolve(__dirname, 'src/lib/index.js'),
+          name: 'DaneshUI',
+          fileName: (format) => `index.${format === 'es' ? 'mjs' : 'js'}`,
+          formats: ['es', 'cjs'],
+        },
+        rollupOptions: {
+          external: ['react', 'react-dom', 'react/jsx-runtime', 'daneshicons'],
+          output: {
+            globals: {
+              react: 'React',
+              'react-dom': 'ReactDOM',
+              'react/jsx-runtime': 'jsxRuntime',
+              daneshicons: 'DaneshIcons',
+            },
           },
         },
       },
-    },
+    }
+  }
+
+  // Default app build & serve
+  return {
+    plugins: [react(), tailwindcss()],
   }
 })
